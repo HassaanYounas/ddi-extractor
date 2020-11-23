ec9184d920b55beb24c474de152350226217e10b
Hassaan Younas <hassaanyounas123@gmail.com>
2020-11-23 14:44:42 +0500

    NER done.

--- ddi.py
+++ ddi.py
@@ -1,14 +1,23 @@
-import os
+import os, json
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 
 java_path = 'C:/Program Files/Java/jre1.8.0_271/bin'
 os.environ['JAVAHOME'] = java_path
 
-ner = StanfordNERTagger(
-    './ai-models/ner-model.ser.gz', 
-    './java/stanford-ner.jar', 
-    encoding = 'utf-8'
-)
+def ner(text):
+    ner = StanfordNERTagger(
+        './ai-models/ner-model.ser.gz', 
+        './java/stanford-ner.jar', 
+        encoding = 'utf-8'
+    )
+    drugs = { 'drug': [], 'drug-n': [], 'group': [], 'brand': [] }
+    tags = ner.tag(word_tokenize(text))
+    for token in tags:
+        if token[1] is not 'O':
+            drugs[token[1]].append({ 'name': token[0] })
+    return drugs
 
-print(ner.tag(word_tokenize('The fluoroquinolones for urinary tract infections: a review.')))
+def re(text):
+    
+    return
--- server.py
+++ server.py
@@ -1,6 +1,27 @@
-from flask import Flask
+import json, ddi
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api/ner', methods=['GET'])
+def ner():
+    drugs = ddi.ner(request.get_json().get('text', ''))
+    response = app.response_class(
+        response = json.dumps(drugs),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
+
+@app.route('/api/re', methods=['GET'])
+def re():
+    drugs = ddi.ner(request.get_json().get('text', ''))
+    response = app.response_class(
+        response = json.dumps(drugs),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+    app.run(debug = True)
